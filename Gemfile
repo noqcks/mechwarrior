15104e46bd4c5f61f850aa4c7bcb889862965827
Ben Visser <theodore.r.visser@gmail.com>
2015-04-08 19:51:41 -0700

    finished with whatcd and brokenstones. might add support for non-gazelle sites later

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'commander', '4.2.1'
 gem 'command_line_reporter', '3.3.5'
+gem 'commander', '4.3.2'
 gem 'mechanize', '2.7.3'
--- mechwarrior.rb
+++ mechwarrior.rb
@@ -4,6 +4,7 @@ require 'commander/import'
 require 'yaml'
 require 'command_line_reporter'
 require 'mechanize'
+require 'json'
 
 program :name, 'Mechwarrior'
 program :version, '0.0.1'
@@ -16,31 +17,28 @@ command :search do |c|
   c.option '-t', '--tags STRING', String, 'A comma separated list of tags'
   c.option '-f', '--filelist STRING', String, 'A string to search in file list'
   c.option '-d', '--description STRING', String, 'A string to search torrent description'
-  # c.option '-ob', '--orderby STRING', String, 'What to order the search results by'
-  # c.option '-ow', '--orderway STRING', String, 'How to order the search results'
-  # c.option '--sites LIST', String, 'A list of the sites to search'
+  c.option '-s', '--sites LIST', Array, 'A list of sites to search'
+  # gazelle sites
   c.action do |args, options|
     agent = Mechanize.new { |agent| agent.user_agent_alias = 'Mac Safari' }
-
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     config = YAML.load_file('sites.yml')
-
     config.each do |site|
-      site = site[1]
-      base_url = site["login"].split(/login/)[0]
+      $site = site[1]
+      $base_url = $site["login"].split(/login/)[0]
+      if (options.sites == nil) || (options.sites.include? $site)
+        page = agent.post($site["login"], {
+          "username": $site["username"],
+          "password": $site["password"],
+          "loginSubmit": "Login"
+        })
+        params = "&year=#{options.year}&taglist=#{options.tags}&description=#{options.description}"
+        page = agent.get("#{$site["search"]}#{args[0]}#{params}")
 
-      page = agent.post(site["login"], {
-        "username": site["username"],
-        "password": site["password"],
-        "loginSubmit": "Login"
-      })
-      params = "&year=#{options.year}&taglist=#{options.tags}&description=#{options.description}"
-      page = agent.get("#{site["search"]}#{args[0]}#{params}")
-
-      p page.search('.group').text.gsub(/[\t\n]/, "").inspect
+        results = JSON.parse(page.body)['response']['results']
+        Table.new.puts(results)
+      end
     end
-
-    # Table.new.puts(search_obj)
-
   end
 end
 
@@ -50,41 +48,37 @@ command :init do |c|
   c.action do |args, options|
     config = YAML.load_file('sites.yml')
     config.each do |site|
-      agree("#{site[0]}? ")
-      username = ask 'Username: ', String
-      password = ask('Password: ', String) { |q| q.echo = "*" }
-      site[1]["username"] = "#{username.to_s}"
-      site[1]["password"] = "#{password.to_s}"
-      File.open('sites.yml', 'w') {|f| f.write config.to_yaml }
+      say "Do you have access to:"
+      if agree("#{site[0]}? ") === true
+        username = ask 'Username: ', String
+        password = ask('Password: ', String) { |q| q.echo = "*" }
+        site[1]["username"] = "#{username.to_s}"
+        site[1]["password"] = "#{password.to_s}"
+        File.open('sites.yml', 'w') {|f| f.write config.to_yaml }
+      end
     end
   end
 end
 
 class Table
   include CommandLineReporter
-
-   def puts(json_obj)
-    table(:border => true) do
+  def puts(json_obj)
+    config = YAML.load_file('sites.yml')
+    table(:border => true) do 
       row do
-        column('ARTIST', :width => 20)
-        column('ALBUM', :width => 30)
-        column('YEAR', :width => 15)
-        column('SIZE', :width => 15)
-        column('SNATCHED', :width => 15)
-        column('SEEDERS', :width => 15)
-        column('LEECHERS', :width => 15)
+        $site['columns'].each do |key,value|
+          column(key, :width => 20)
+        end
+        column('LINK', :width => 50)
       end
-      json_obj.each do
+      json_obj.each do |x|
         row do
-          column()
-          column()
-          column()
-          column()
-          column()
-          column()
-          column()
+          $site['columns'].each do |key,value|
+            column(x[value], :width => 10)
+          end
+          column($base_url + 'torrents.php?id=' + x['groupId'].to_s, :width => 50)
         end
       end
-   end
+    end unless json_obj.empty?
   end
 end
